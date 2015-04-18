d3c72b061e5bd246eee77b22d2df5ebed7c918cc
Krishan Patel <krishan@rockolabs.com>
2015-04-18 14:17:59 +0100

    added touch to go to main

--- app/src/main/java/com/twelfthman/app/ChooseMatchActivity.java
+++ app/src/main/java/com/twelfthman/app/ChooseMatchActivity.java
@@ -1,7 +1,11 @@
 package com.twelfthman.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ChooseMatchActivity extends ActionBarActivity
 {
@@ -11,6 +15,20 @@ public class ChooseMatchActivity extends ActionBarActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_match);
+        ButterKnife.inject(this);
+    }
+
+    @OnClick(R.id.img_back)
+    public void onImageClicked()
+    {
+        Log.i("ChooseMatchActivity", "onImageClicked");
+        gotoMain();
+    }
+
+    private void gotoMain()
+    {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 
 }
