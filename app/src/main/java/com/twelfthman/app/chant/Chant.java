ec09f9c62772df65f70571bb55ca85bdbc3131dc
Dhilip Buddha <dhilipb@gmail.com>
2015-04-18 16:10:37 +0100

    Added icons and Chant shizzle

---
+++ app/src/main/java/com/twelfthman/app/chant/Chant.java
@@ -0,0 +1,35 @@
+package com.twelfthman.app.chant;
+
+/**
+ * Created by dhilipb on 18/04/2015.
+ */
+public class Chant {
+
+    private String team;
+    private String title;
+    private String lyrics;
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLyrics() {
+        return lyrics;
+    }
+
+    public void setLyrics(String lyrics) {
+        this.lyrics = lyrics;
+    }
+}
